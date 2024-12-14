26a090f20d88c00d0e341bf4d04ea89b7cf504fa
Brent Spector <9450120+brentspector@users.noreply.github.com>
2024-12-14 10:37:48 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tabletop Slay the Spire Tierlist
+
+## Coming soon
+
+## Dev Tips
+Start by running `ionic serve --external` to see the webpage
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TT SlayTiers',
+  webDir: 'dist'
+};
+
+export default config;
