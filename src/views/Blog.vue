7de482f72f981cb509de34cf14985d7d7a927010
Brent Spector <9450120+brentspector@users.noreply.github.com>
2025-01-23 01:48:16 +0000

    Update suport for Docker

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,55 +1,49 @@
 <template>
-  <div>
-    <h1>Blog</h1>
-    <div v-for="post in posts" :key="post.sys.id" class="post">
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Blog</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <div v-for="post in posts" :key="post.sys.id" class="post">
       <h2>{{ post.fields.title }}</h2>
       <p>{{ post.fields.summary }}</p>
     </div>
     <infinite-loading @infinite="loadMorePosts" />
-  </div>
+    </ion-content>
+  </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script lang="ts" setup>
+import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
+import { ref } from 'vue'
 import client from '@/contentful'
 import InfiniteLoading from 'v3-infinite-loading'
 import 'v3-infinite-loading/lib/style.css'
 
-export default defineComponent({
-  name: 'Blog',
-  components: {
-    InfiniteLoading
-  },
-  setup() {
-    const posts = ref<contentful.Entry<any>[]>([])
-    const skip = ref(0)
-    const limit = 10
+const posts = ref<Array<any>>([])
+const skip = ref(0)
+const limit = 10
 
-    const loadMorePosts = async ($state: { loaded: () => void; complete: () => void; }) => {
-      try {
-        const response = await client.getEntries({
-          skip: skip.value,
-          limit
-        })
-        if (response.items.length) {
-          posts.value.push(...response.items)
-          skip.value += limit
-          $state.loaded()
-        } else {
-          $state.complete()
-        }
-      } catch (error) {
-        console.error(error)
-        $state.complete()
-      }
+const loadMorePosts = async ($state: { loaded: () => void; complete: () => void; }) => {
+  try {
+    const response = await client.getEntries({
+      skip: skip.value,
+      limit
+    })
+    if (response.items.length) {
+      posts.value.push(...response.items)
+      skip.value += limit
+      $state.loaded()
+    } else {
+      $state.complete()
     }
-
-    return {
-      posts,
-      loadMorePosts
-    }
+  } catch (error) {
+    console.error(error)
+    $state.complete()
   }
-})
+}
 </script>
 
 <style scoped>
--- src/views/SearchList.vue
+++ src/views/SearchList.vue
@@ -32,95 +32,67 @@
     </ion-page>
   </template>
   
-  <script lang="ts">
-  import {
-    IonContent,
-    IonFab,
-    IonFabButton,
-    IonHeader,
-    IonItem,
-    IonList,
-    IonPage,
-    IonSearchbar,
-    IonTitle,
-    IonToolbar,
-    modalController
-  } from "@ionic/vue";
-  import { defineComponent, ref } from "vue";
-  
-  export default defineComponent({
-    components: {
+  <script lang="ts" setup>
+    import {
       IonContent,
       IonFab,
       IonFabButton,
       IonHeader,
-      IonList,
       IonItem,
+      IonList,
       IonPage,
       IonSearchbar,
       IonTitle,
       IonToolbar,
       modalController
-    },
-    props: {
-      content: {
-        type: Object as () => Record<string, any>[],
-        required: true,
-      },
-      contentKey: {
-        type: String,
-        required: true,
-      },
-      label: {
-        type: String,
-        required: true,
-      },
-    },
-    setup(props) {
-      const searchQuery = ref("");
-      const searchResults = () => {
-        if (searchQuery.value) {
-          console.log(props.contentKey);
-          return props.content.filter((item) => {
-            if (props.contentKey) {
-              return (
-                item[props.contentKey]
-                  .toLowerCase()
-                  .indexOf(searchQuery.value.toLowerCase()) != -1
-              );
-            } else {
-              console.log(item);
-              return (
-                item.toLowerCase().indexOf(searchQuery.value.toLowerCase()) != -1
-              );
-            }
-          });
-        } else {
-          return props.content;
-        }
-      };
-      const getValue = (item: Record<string, any>) => {
-        if (props.contentKey) {
-          return item[props.contentKey];
-        } else {
-          return item;
-        }
-      };
-      const getLoopKey = (value: Record<string, any>) => {
-        return value[props.contentKey];
+    } from "@ionic/vue";
+    import { ref } from "vue";
+    
+    const props = defineProps<{
+      content: Record<string, any>[],
+      contentKey: string,
+      label: string
+    }>();
+
+    const searchQuery = ref("");
+
+    const searchResults = () => {
+      if (searchQuery.value) {
+        //console.log(props.contentKey);
+        return props.content.filter((item) => {
+          if (props.contentKey) {
+            return (
+              item[props.contentKey]
+                .toLowerCase()
+                .indexOf(searchQuery.value.toLowerCase()) != -1
+            );
+          } else {
+            console.log(item);
+            return (
+              item.toLowerCase().indexOf(searchQuery.value.toLowerCase()) != -1
+            );
+          }
+        });
+      } else {
+        return props.content;
       }
-      const confirm = (value: any) => modalController.dismiss(value);
-      const cancel = () => modalController.dismiss();
-      return {
-        confirm,
-        cancel,
-        getLoopKey,
-        getValue,
-        searchQuery,
-        searchResults,
-      };
-    },
-  });
+    };
+
+    const getValue = (item: Record<string, any>) => {
+      if (props.contentKey) {
+        return item[props.contentKey];
+      } else {
+        return item;
+      }
+    };
+
+    const getLoopKey = (value: Record<string, any>) => {
+      return value[props.contentKey];
+    };
+    
+    const confirm = (value: any) => modalController.dismiss(value);
+    const cancel = () => modalController.dismiss();
+
   </script>
   <!-- GLOBAL STYLES
       AVOID USING THIS -->
